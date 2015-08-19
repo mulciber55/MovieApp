66a4d2017551e9a6a6f59999cbe07d99461ac644
Borys <borysstach@gmail.com>
2015-08-19 13:36:18 +0200

    Testing Adpater to View

--- app/src/main/java/com/example/android/moviesapp/MainActivityFragment.java
+++ app/src/main/java/com/example/android/moviesapp/MainActivityFragment.java
@@ -1,15 +1,24 @@
 package com.example.android.moviesapp;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    static final String[] NUMBERS = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
 
     public MainActivityFragment() {
     }
@@ -17,8 +26,15 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Adap
+       ArrayAdapter<String> movieGridAdapter = new ArrayAdapter<String>(
+               getActivity(),
+               R.layout.movie_poster,
+               R.id.poster_tex_view,
+               NUMBERS);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.main_grid_view);
+        gridView.setAdapter(movieGridAdapter);
 
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
     }
 }
