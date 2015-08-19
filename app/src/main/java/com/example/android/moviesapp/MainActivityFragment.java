267c0dff264cefbb83a2eda4664fcfec0dff0072
Borys <borysstach@gmail.com>
2015-08-19 16:23:50 +0200

    Add ActiveSync Class witch download and parse poster images path

--- app/src/main/java/com/example/android/moviesapp/MainActivityFragment.java
+++ app/src/main/java/com/example/android/moviesapp/MainActivityFragment.java
@@ -1,19 +1,35 @@
 package com.example.android.moviesapp;
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
 
-    static final String[] NUMBERS = new String[] {
+    ArrayAdapter<String> movieGridAdapter;
+    static final String[] NUMBERS = new String[]{
             "A", "B", "C", "D", "E",
             "F", "G", "H", "I", "J",
             "K", "L", "M", "N", "O",
@@ -26,15 +42,147 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-       ArrayAdapter<String> movieGridAdapter = new ArrayAdapter<String>(
-               getActivity(),
-               R.layout.movie_poster,
-               R.id.poster_tex_view,
-               NUMBERS);
+        //Making Adapter witch connect every strings from NUMBER, with correct TextView on movie_poster layout
+        movieGridAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.movie_poster,
+                R.id.poster_tex_view,
+                new ArrayList<String>());
+        // connect this fragment class with xml file
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        //make reference to GridView
         GridView gridView = (GridView) rootView.findViewById(R.id.main_grid_view);
+        //set Adapter on gridView
         gridView.setAdapter(movieGridAdapter);
+
 
         return rootView;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FetchMovies fetchMovies = new FetchMovies();
+        fetchMovies.execute("popularity");
+    }
+
+    public class FetchMovies extends AsyncTask<String, Void, String[]> {
+
+        private final String LOG_TAG = FetchMovies.class.getSimpleName();
+
+        /**
+         * method parse JSON from API
+         *
+         * @param jsonString JSON string downloaded from Movie API
+         * @return table with poster patch to build URL
+         * @throws JSONException
+         */
+        private String[] getMovieDataFromJson(String jsonString)
+                throws JSONException {
+            final String RESULTS = "results";
+            final String POSTER_PATH = "backdrop_path";
+            //final String TITLE = "title";
+            //final String ORIGINAL_TITLE = "original_title";
+            //final String RATING = "vote_average";
+            //final String RELEASE_DATE = "release_date";
+
+            JSONObject movieJson = new JSONObject(jsonString);
+            JSONArray movieArray = movieJson.getJSONArray(RESULTS);
+
+            String[] resultStr = new String[20];
+            for (int i = 0; i < movieArray.length(); i++) {
+                resultStr[i] = movieArray.getJSONObject(i).getString(POSTER_PATH);
+            }
+            return resultStr;
+        }
+
+        @Override
+        protected String[] doInBackground(String... param) {
+
+            //needed references to connect with Movie API
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String movieJsonStr = null;
+
+            try {
+                //construct URL to connect with Movie API
+                final String BASE_URL = "https://api.themoviedb.org/3/discover/movie";
+                final String API_KEY = "api_key";
+                final String YOUR_API_KEY = "!!!PLACE YOUR KEY!!!!";
+                final String SORT = "sort_by";
+                final String POPULARITY = "popularity.desc";
+                final String RATE = "vote_average.desc";
+
+                Uri builtUri = Uri.parse(BASE_URL).buildUpon()
+                        .appendQueryParameter(API_KEY, YOUR_API_KEY)
+                        .appendQueryParameter(SORT, POPULARITY)
+                        .build();
+                URL url = new URL(builtUri.toString());
+
+                //Open connection with our url
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                //Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    return null;
+                }
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line);
+                }
+
+                if (buffer.length() == 0) {
+                    return null;
+                }
+
+
+                movieJsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            try {
+                return getMovieDataFromJson(movieJsonStr);
+            } catch (JSONException e) {
+                Log.e(LOG_TAG, e.getMessage(), e);
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        protected void onPostExecute(String[] result) {
+            if (result != null) {
+                movieGridAdapter.clear();
+                for (String moviePoster : result) {
+                    movieGridAdapter.add(moviePoster);
+                }
+            }
+
+        }
+    }
+
 }
